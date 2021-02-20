023ebb929d4ec2b00b33a34e79f2890132c5c1a7
Molly Fuhrman <mollyfuhrman@gmail.com>
2021-02-19 19:05:35 -0800

    Received data from API.

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,8 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const App = () => {
+
+  const [start, useStartDate] = useState(null);
+  const [end, useEndDate] = useState(null);
+
+  useEffect(() => {
+    console.log(start, end)
+    if (start && end) {
+      axios.get(`https://api.coindesk.com/v1/bpi/historical/close.json?start=${start}&end=${end}`)
+      .then((data) => {
+        console.log(data)
+      })
+    }
+  }, [start, end])
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    useStartDate(e.target.startDate.value);
+    useEndDate(e.target.endDate.value);
+  }
   return (
-    <h1>Hello from inside App!</h1>
+    <form onSubmit={handleSubmit}>
+      <label>Start date:</label>
+      <input type="date" id="startDate" />
+      <label>End date:</label>
+      <input type="date" id="endDate" />
+      <input type="submit" value="Submit" />
+    </form>
   )
 }
 
--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const bodyParser = require('body-parser');
+
+bodyParser.json();
 
 app.use('/', express.static('dist'));
 
-// app.use('/', (req, res) => {
-//   res.render();
-// });
+app.use((req, res) => {
+  res.header("Access-Control-Allow-Origin", "https://api.coindesk.com");
+});
 
 let PORT = 3000;
 
